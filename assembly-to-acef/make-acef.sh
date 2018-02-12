3411621e8ae1cd1f5d36031847234fdd2694a5c2
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-02-12 12:00:03 +0100

    Added date-time to messages

--- assembly-to-acef/make-acef.sh
+++ assembly-to-acef/make-acef.sh
@@ -2,21 +2,22 @@
 
 my_dir=$(pwd)
 
-db_user=col
-db_password=col
-db_name=Assembly_Global
-
 rm -rf /tmp/acef
 mkdir /tmp/acef
+if [ ${?} != 0 ]
+then
+	echo "Could not create temp directory /tmp/acef"
+	exit 1
+fi
 
 database_id="${1}"
 
 if [ "x${database_id}" = "x" ]
 then
-	echo "Retrieving GSD IDs ..."
-	mysql --user=${db_user} --password=${db_password} ${db_name} -e 'SELECT record_id FROM `databases`' > /tmp/acef/database_ids.txt
-	echo "Creating temp table ..."
-	mysql --user=${db_user} --password=${db_password} ${db_name} < once.sql
+	echo "$(date '+%Y-%m-%d %H:%M:%S') Retrieving GSD IDs ..."
+	mysql --defaults-extra-file=my.cnf -e 'SELECT record_id FROM `databases`' > /tmp/acef/database_ids.txt
+	echo "$(date '+%Y-%m-%d %H:%M:%S') Creating temp table ..."
+	mysql --defaults-extra-file=my.cnf < once.sql
 else
 	echo $database_id > /tmp/acef/database_ids.txt
 fi
@@ -28,14 +29,15 @@ do
 		# Skip header
 		continue
 	fi
-    echo "Processing database: ${line}"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Processing database: ${line}"
 	mkdir /tmp/acef/${line}
-	chmod -R 777 /tmp/acef/$line
-    sed "s/__OUTPUT_DIR__/\/tmp\/acef\/$line/g" assembly_db_to_acef.sql > /tmp/acef/temp.sql
-    sed -i "s/__DATABASE_ID__/$line/g" /tmp/acef/temp.sql 
-    mysql --user=${db_user} --password=${db_password} ${db_name} < /tmp/acef/temp.sql
-    cd "/tmp/acef/$line"
+	chmod -R 777 /tmp/acef/${line}
+    sed "s/__OUTPUT_DIR__/\/tmp\/acef\/${line}/g" assembly_db_to_acef.sql > /tmp/acef/temp.sql
+    sed -i "s/__DATABASE_ID__/${line}/g" /tmp/acef/temp.sql 
+    mysql --defaults-extra-file=my.cnf < /tmp/acef/temp.sql
+    cd "/tmp/acef/${line}"
     zip_file="/tmp/acef/${line}.tar.gz"
     tar czf ${zip_file} *.txt
+    cd ${my_dir}
     mv ${zip_file} ${my_dir}/zips
 done < /tmp/acef/database_ids.txt
