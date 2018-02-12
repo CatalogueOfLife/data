82fcc1739de946cacca2785ceb9a900b0cda7fab
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-02-12 10:51:18 +0100

    Completed make-acef.sh script

--- assembly-to-acef/make-acef.sh
+++ assembly-to-acef/make-acef.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+my_dir=$(pwd)
+
 db_user=col
 db_password=col
 db_name=Assembly_Global
@@ -7,28 +9,33 @@ db_name=Assembly_Global
 rm -rf /tmp/acef
 mkdir /tmp/acef
 
-
 database_id="${1}"
 
 if [ "x${database_id}" = "x" ]
 then
-	mysql --user=${db_user} --password=${db_password} ${db_name} -e 'SELECT record_id AS `FOO` from `databases`' > /tmp/acef/database_ids.txt
+	echo "Retrieving GSD IDs ..."
+	mysql --user=${db_user} --password=${db_password} ${db_name} -e 'SELECT record_id FROM `databases`' > /tmp/acef/database_ids.txt
+	echo "Creating temp table ..."
+	mysql --user=${db_user} --password=${db_password} ${db_name} < once.sql
 else
 	echo $database_id > /tmp/acef/database_ids.txt
 fi
 
-while read -r line || [[ -n "$line" ]]
+while read -r line
 do
-	if [ "$line" = FOO ]
+	if [ "${line}" = "record_id" ]
 	then
+		# Skip header
 		continue
 	fi
-    echo "Processing database: $line"
-	mkdir /tmp/acef/$line
+    echo "Processing database: ${line}"
+	mkdir /tmp/acef/${line}
 	chmod -R 777 /tmp/acef/$line
     sed "s/__OUTPUT_DIR__/\/tmp\/acef\/$line/g" assembly_db_to_acef.sql > /tmp/acef/temp.sql
     sed -i "s/__DATABASE_ID__/$line/g" /tmp/acef/temp.sql 
     mysql --user=${db_user} --password=${db_password} ${db_name} < /tmp/acef/temp.sql
     cd "/tmp/acef/$line"
-    tar czf "/tmp/acef/$line.tar.gz" *.txt
+    zip_file="/tmp/acef/${line}.tar.gz"
+    tar czf ${zip_file} *.txt
+    mv ${zip_file} ${my_dir}/zips
 done < /tmp/acef/database_ids.txt
