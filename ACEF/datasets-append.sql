59d3bb07aa0ea93741d790e8d2efc4fe6558ec71
mdoering <mdoering@gbif.org>
2018-03-01 11:10:28 +0100

    Update dataset.sql generation

--- ACEF/datasets-append.sql
+++ ACEF/datasets-append.sql
@@ -1,14 +1,17 @@
 
-UPDATE dataset SET data_access = 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/assembly/' || key || '.tar.gz' WHERE key < 1000;
+UPDATE dataset SET data_format=1, data_access = 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/assembly/' || key || '.tar.gz' WHERE key < 1000;
 ALTER SEQUENCE dataset_key_seq RESTART WITH 1000;
 
 -- removed, old sources which we mark as deleted
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('117', 'chenobase', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('159', 'fada_copepoda', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('135', 'fada_turbellaria', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('165', 'faeu_turbellaria', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('16',  'iopi-gpc', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('43',  'lecypages', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('56',  'lhd', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('64',  'solanaceae_source', now(), now());
-INSERT INTO dataset (data_format, key, title, created, deleted) VALUES ('60',  'worms_proseriata-kalyptorhynchia', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('1',   'col_management_classification', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('2',   'species_2000_common_names', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('117', 'chenobase', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('159', 'fada_copepoda', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('135', 'fada_turbellaria', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('165', 'faeu_turbellaria', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('16',  'iopi-gpc', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('43',  'lecypages', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('56',  'lhd', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('64',  'solanaceae_source', now(), now());
+INSERT INTO dataset (key, title, created, deleted) VALUES ('60',  'worms_proseriata-kalyptorhynchia', now(), now());
+
