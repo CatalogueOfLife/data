373fe5e8040cf9449ca29b8db5a61f5a30da1bc9
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-06-28 15:03:30 +0200

    Genus and lower now excluded from higher classification dwca

--- ACEF/higher-classification.sql
+++ ACEF/higher-classification.sql
@@ -5,3 +5,16 @@ SELECT t.taxon_id AS id
 , t.total_species_estimation AS speciesEstimate
 , REPLACE(REPLACE(REPLACE(IFNULL(t.estimate_source,''), '\n', ' '), '\r', ' '), '  ', ' ') AS speciesEstimateReference
 FROM ac_latest._taxon_tree AS t
+WHERE (t.rank IN ('kingdom', 'phylum', 'order', 'class', 'superfamily', 'family'))
+OR (t.rank = 'not assigned' AND INSTR(t.name, ' ') = 0)
+ORDER BY (
+	CASE t.rank
+		WHEN 'kingdom' THEN 0
+		WHEN 'phylum' THEN 1
+		WHEN 'order' THEN 2
+		WHEN 'class' THEN 3
+		WHEN 'superfamily' THEN 4
+		WHEN 'family' THEN 5
+		ELSE /* Shouldn't happen! */ -1
+	END
+), t.name
