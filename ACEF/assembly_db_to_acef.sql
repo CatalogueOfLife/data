8789f6021ed922ddc643b7451621e4e9b1d6a1b5
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-07-02 15:05:27 +0200

    dataset 7 (common names) now has special arrangements

--- ACEF/assembly_db_to_acef.sql
+++ ACEF/assembly_db_to_acef.sql
@@ -6,6 +6,19 @@
  * __OUTPUT_DIR__ and __DATABASE_ID__ with proper values.
  */
 
+DROP FUNCTION IF EXISTS N2E;
+CREATE FUNCTION N2E(s VARCHAR(512)) RETURNS VARCHAR(512) DETERMINISTIC
+	RETURN IFNULL(s,'');
+
+DROP FUNCTION IF EXISTS CLEAN_STR;
+CREATE FUNCTION CLEAN_STR(s VARCHAR(512)) RETURNS VARCHAR(512) DETERMINISTIC
+  RETURN REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(N2E(s),'\r',' '),'\n',' '),'\t',' '),'  ',' '),'  ',' '),'  ',' ');
+
+# There is a special dataset just for common names, pointing to
+# species outside that dataset. It needs special treatment
+SET @COMMON_NAMES_DB = 7;
+
+
 /* 
  * AcceptedSpecies.txt
  */
@@ -14,29 +27,29 @@ SELECT
 'SpeciesEpithet', 'AuthorString', 'GSDNameStatus', 'Sp2000NameStatus', 'IsExtinct', 'HasPreHolocene',
 'HasModern', 'LifeZone', 'AdditionalData', 'LTSSpecialist', 'LTSDate','SpeciesURL', 'GSDTaxonGUID', 'GSDNameGUID'
 UNION
-SELECT sn.name_code							AS AcceptedTaxonID
-,	IFNULL(fam.kingdom,'')					AS Kingdom
-,	IFNULL(fam.phylum,'')					AS Phylum
-,	IFNULL(fam.class,'')					AS Class
-,	IFNULL(fam.`order`,'')					AS `Order`
-,	IFNULL(fam.superfamily,'')				AS Superfamily
-,	IFNULL(fam.family,'')					AS Family
-,	IFNULL(sn.genus,'')						AS Genus
-,	IFNULL(sn.subgenus,'')					AS SubGenusName
-,	IFNULL(sn.species,'')					AS SpeciesEpithet
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(sn.author, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS AuthorString
-,	''										AS GSDNameStatus /* ??? */
-,	IFNULL(nomstatus.sp2000_status,'')		AS Sp2000NameStatus
-,	IFNULL(sn.is_extinct,'')				AS IsExtinct
-,	IFNULL(sn.has_preholocene,'')			AS HasPreHolocene
-,	IFNULL(sn.has_modern,'')				AS HasModern
-,	IFNULL(lz.lifezones,'')					AS LifeZone
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(sn.comment, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS AdditionalData
-,	IFNULL(sp.specialist_name,'')			AS LTSSpecialist
-,	IFNULL(sn.scrutiny_date,'')				AS LTSDate
-,	IFNULL(sn.web_site,'')					AS SpeciesURL
-,	IFNULL(sn.GSDTaxonGUID,'')				AS GSDTaxonGUID
-,	IFNULL(sn.GSDNameGUID,'')				AS GSDNameGUID
+SELECT sn.name_code					AS AcceptedTaxonID
+,	N2E(fam.kingdom)				AS Kingdom
+,	N2E(fam.phylum)					AS Phylum
+,	N2E(fam.class)					AS Class
+,	N2E(fam.`order`)				AS `Order`
+,	N2E(fam.superfamily)			AS Superfamily
+,	N2E(fam.family)					AS Family
+,	N2E(sn.genus)					AS Genus
+,	N2E(sn.subgenus)				AS SubGenusName
+,	N2E(sn.species)					AS SpeciesEpithet
+,	CLEAN_STR(sn.author) 			AS AuthorString
+,	''								AS GSDNameStatus /* ??? */
+,	N2E(nomstatus.sp2000_status)	AS Sp2000NameStatus
+,	N2E(sn.is_extinct)				AS IsExtinct
+,	N2E(sn.has_preholocene)			AS HasPreHolocene
+,	N2E(sn.has_modern)				AS HasModern
+,	N2E(lz.lifezones)				AS LifeZone
+,	CLEAN_STR(sn.comment) 			AS AdditionalData
+,	N2E(sp.specialist_name)			AS LTSSpecialist
+,	N2E(sn.scrutiny_date)			AS LTSDate
+,	N2E(sn.web_site)				AS SpeciesURL
+,	N2E(sn.GSDTaxonGUID)			AS GSDTaxonGUID
+,	N2E(sn.GSDNameGUID)				AS GSDNameGUID
 INTO OUTFILE '__OUTPUT_DIR__/AcceptedSpecies.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -50,7 +63,7 @@ LEFT JOIN lifezones_per_name lz ON (sn.record_id = lz.scientific_name_id)
 LEFT JOIN specialists AS sp ON (sn.specialist_id = sp.record_id)
 WHERE (sn.infraspecies_marker IS NULL OR infraspecies_marker = '')
 AND sn.is_accepted_name != 0 /* both 1 and 5 are accepted names; 5 likely data corruption */
-AND sn.database_id=__DATABASE_ID__;
+AND (sn.database_id=__DATABASE_ID__ OR (__DATABASE_ID__=@COMMON_NAMES_DB AND sn.name_code IN (SELECT name_code FROM common_names WHERE database_id = @COMMON_NAMES_DB));
 
 
 /* 
@@ -60,23 +73,23 @@ SELECT 'AcceptedTaxonID','parentID','InfraSpeciesEpithet','InfraSpeciesMarker','
  'GSDNameStatus','Sp2000NameStatus','IsExtinct','HasPreHolocene','HasModern','LifeZone','AdditionalData',
  'LTSSpecialist','LTSDate','InfraSpeciesURL','GSDTaxonGUID','GSDNameGUID'
 UNION
-SELECT sn.name_code							AS AcceptedTaxonID
-,	IFNULL(sn.infraspecies_parent_name_code,'') AS parentID
-,	IFNULL(sn.infraspecies,'')				AS InfraSpeciesEpithet
-,	IFNULL(sn.infraspecies_marker,'')		AS InfraSpeciesMarker
-,	IFNULL(sn.author,'')					AS InfraSpeciesAuthorString
-,	''										AS GSDNameStatus /* ??? */
-,	IFNULL(nomstatus.sp2000_status,'')		AS Sp2000NameStatus
-,	IFNULL(sn.is_extinct,'')				AS IsExtinct
-,	IFNULL(sn.has_preholocene,'')			AS HasPreHolocene
-,	IFNULL(sn.has_modern,'')				AS HasModern
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(lz.lifezones, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS LifeZone
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(sn.comment, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS AdditionalData
-,	IFNULL(sp.specialist_name,'')			AS LTSSpecialist
-,	IFNULL(sn.scrutiny_date,'')				AS LTSDate
-,	IFNULL(sn.web_site,'')					AS InfraSpeciesURL
-,	IFNULL(sn.GSDTaxonGUID,'')				AS GSDTaxonGUID
-,	IFNULL(sn.GSDNameGUID,'')				AS GSDNameGUID
+SELECT sn.name_code					AS AcceptedTaxonID
+,	N2E(sn.infraspecies_parent_name_code)	AS parentID
+,	N2E(sn.infraspecies)			AS InfraSpeciesEpithet
+,	N2E(sn.infraspecies_marker)		AS InfraSpeciesMarker
+,	N2E(sn.author)					AS InfraSpeciesAuthorString
+,	''								AS GSDNameStatus /* ??? */
+,	N2E(nomstatus.sp2000_status)	AS Sp2000NameStatus
+,	N2E(sn.is_extinct)				AS IsExtinct
+,	N2E(sn.has_preholocene)			AS HasPreHolocene
+,	N2E(sn.has_modern)				AS HasModern
+,	CLEAN_STR(lz.lifezones)			AS LifeZone
+,	CLEAN_STR(sn.comment)			AS AdditionalData
+,	N2E(sp.specialist_name)			AS LTSSpecialist
+,	N2E(sn.scrutiny_date)			AS LTSDate
+,	N2E(sn.web_site)				AS InfraSpeciesURL
+,	N2E(sn.GSDTaxonGUID)			AS GSDTaxonGUID
+,	N2E(sn.GSDNameGUID)				AS GSDNameGUID
 INTO OUTFILE '__OUTPUT_DIR__/AcceptedInfraspecificTaxa.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -89,7 +102,7 @@ LEFT JOIN lifezones_per_name lz ON (sn.record_id = lz.scientific_name_id)
 LEFT JOIN specialists AS sp ON (sn.specialist_id = sp.record_id)
 WHERE (sn.infraspecies_marker IS NOT NULL AND infraspecies_marker != '')
 AND sn.is_accepted_name != 0
-AND sn.database_id=__DATABASE_ID__;
+AND (sn.database_id=__DATABASE_ID__ OR (__DATABASE_ID__=@COMMON_NAMES_DB AND sn.name_code IN (SELECT name_code FROM common_names WHERE database_id = @COMMON_NAMES_DB));
 
 
 /* 
@@ -98,18 +111,18 @@ AND sn.database_id=__DATABASE_ID__;
 SELECT 'ID','AcceptedTaxonID','Genus','SubGenusName','SpeciesEpithet','AuthorString','InfraSpeciesEpithet',
 'InfraSpeciesMarker','InfraSpeciesAuthorString','GSDNameStatus','Sp2000NameStatus','GSDNameGUID'
 UNION
-SELECT sn.name_code							AS ID
-,	sn.accepted_name_code					AS AcceptedTaxonID
-,	IFNULL(sn.genus,'')						AS Genus
-,	IFNULL(sn.subgenus,'')					AS SubGenusName
-,	IFNULL(sn.species,'')					AS SpeciesEpithet
-,	IFNULL(sn.author,'')					AS AuthorString
-,	IFNULL(sn.infraspecies,'')				AS InfraSpeciesEpithet
-,	IFNULL(sn.infraspecies_marker,'')		AS InfraSpeciesMarker
-,	''										AS InfraSpeciesAuthorString /* ??? */
-,	''										AS GSDNameStatus /* ??? */
-,	IFNULL(nomstatus.sp2000_status,'')		AS Sp2000NameStatus
-,	IFNULL(sn.GSDNameGUID,'')				AS GSDNameGUID
+SELECT sn.name_code					AS ID
+,	N2E(sn.accepted_name_code)		AS AcceptedTaxonID
+,	N2E(sn.genus)					AS Genus
+,	N2E(sn.subgenus)				AS SubGenusName
+,	N2E(sn.species)					AS SpeciesEpithet
+,	N2E(sn.author)					AS AuthorString
+,	N2E(sn.infraspecies)			AS InfraSpeciesEpithet
+,	N2E(sn.infraspecies_marker)		AS InfraSpeciesMarker
+,	''								AS InfraSpeciesAuthorString /* ??? */
+,	''								AS GSDNameStatus /* ??? */
+,	N2E(nomstatus.sp2000_status)	AS Sp2000NameStatus
+,	N2E(sn.GSDNameGUID)				AS GSDNameGUID
 INTO OUTFILE '__OUTPUT_DIR__/Synonyms.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -127,13 +140,13 @@ AND sn.database_id=__DATABASE_ID__;
  */
 SELECT 'AcceptedTaxonID','CommonName','TransliteratedNames','Language','Country','Area','ReferenceID'
 UNION
-SELECT cn.name_code							AS AcceptedTaxonID
-,	REPLACE(REPLACE(REPLACE(REPLACE(cn.common_name, '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS CommonName
-,	IFNULL(cn.transliteration,'')			AS TransliteratedNames
-,	IFNULL(cn.language,'')					AS Language
-,	IFNULL(cn.country,'')					AS Country
-,	REPLACE(REPLACE(REPLACE(REPLACE(cn.area, '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS Area
-,	IFNULL(cn.reference_code,'')			AS ReferenceID
+SELECT cn.name_code					AS AcceptedTaxonID
+,	CLEAN_STR(cn.common_name)		AS CommonName
+,	N2E(cn.transliteration)			AS TransliteratedNames
+,	N2E(cn.language)				AS Language
+,	N2E(cn.country)					AS Country
+,	CLEAN_STR(cn.area)				AS Area
+,	N2E(cn.reference_code)			AS ReferenceID
 INTO OUTFILE '__OUTPUT_DIR__/CommonNames.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -150,10 +163,10 @@ WHERE cn.database_id=__DATABASE_ID__;
  */
 SELECT 'AcceptedTaxonID','DistributionElement','StandardInUse','DistributionStatus'
 UNION
-SELECT d.name_code							AS AcceptedTaxonID
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(distribution, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS DistributionElement
-,	IFNULL(d.StandardInUse,'')				AS StandardInUse
-,	IFNULL(d.DistributionStatus,'')			AS DistributionStatus
+SELECT d.name_code					AS AcceptedTaxonID
+,	CLEAN_STR(d.distribution)		AS DistributionElement
+,	N2E(d.StandardInUse)			AS StandardInUse
+,	N2E(d.DistributionStatus)		AS DistributionStatus
 INTO OUTFILE '__OUTPUT_DIR__/Distribution.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -169,11 +182,11 @@ WHERE d.database_id=__DATABASE_ID__;
  */
 SELECT 'ReferenceID','Authors','Year','Title','Details'
 UNION
-SELECT r.reference_code						AS ReferenceID
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(r.author, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS Authors
-,	IFNULL(r.year,'')						AS Year
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(r.title, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS Title
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(r.source, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS Details
+SELECT r.reference_code				AS ReferenceID
+,	CLEAN_STR(r.author)				AS Authors
+,	CLEAN_STR(r.year)				AS Year
+,	CLEAN_STR(r.title)				AS Title
+,	CLEAN_STR(r.source)				AS Details
 INTO OUTFILE '__OUTPUT_DIR__/References.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -190,9 +203,9 @@ WHERE r.database_id=__DATABASE_ID__;
  */
 SELECT 'ID','ReferenceType','ReferenceID'
 UNION
-SELECT snr.name_code						AS ID
-,	IFNULL(snr.reference_type,'')			AS ReferenceType
-,	IFNULL(snr.reference_code,'')			AS ReferenceID
+SELECT snr.name_code				AS ID
+,	N2E(snr.reference_type)			AS ReferenceType
+,	N2E(snr.reference_code)			AS ReferenceID
 INTO OUTFILE '__OUTPUT_DIR__/NameReferences.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
@@ -210,21 +223,21 @@ SELECT 'DatabaseFullName','DatabaseName','DatabaseVersion','ReleaseDate','Author
 'GroupNameInEnglish','Abstract','Organization','HomeURL','Coverage','Completeness','Confidence',
 'LogoFileName','ContactPerson'
 UNION
-SELECT db.database_full_name				AS DatabaseFullName
-,	IFNULL(db.database_name,'')				AS DatabaseName
-,	IFNULL(db.version,'')					AS DatabaseVersion
-,	IFNULL(db.release_date,'')				AS ReleaseDate
-,	IFNULL(db.authors_editors,'')			AS AuthorsEditors
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(db.taxonomic_coverage, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS TaxonomicCoverage
-,	''										AS GroupNameInEnglish /* ??? */
-,	REPLACE(REPLACE(REPLACE(REPLACE(IFNULL(db.abstract, ''), '\r\n', ' '), '\n', ' '), '\r', ' '), '\t', ' ') AS Abstract
-,	IFNULL(db.organization,'')				AS Organization
-,	IFNULL(db.web_site,'')					AS HomeURL
-,	IFNULL(db.coverage,'')					AS Coverage
-,	IFNULL(db.completeness,'')				AS Completeness
-,	IFNULL(db.confidence,'')				AS Confidence
-,	''										AS LogoFileName
-,	IFNULL(db.contact_person,'')			AS ContactPerson
+SELECT db.database_full_name		AS DatabaseFullName
+,	N2E(db.database_name)			AS DatabaseName
+,	N2E(db.version)					AS DatabaseVersion
+,	N2E(db.release_date)			AS ReleaseDate
+,	N2E(db.authors_editors)			AS AuthorsEditors
+,	CLEAN_STR(db.taxonomic_coverage)	AS TaxonomicCoverage
+,	''								AS GroupNameInEnglish /* ??? */
+,	CLEAN_STR(db.abstract)			AS Abstract
+,	N2E(db.organization)			AS Organization
+,	N2E(db.web_site)				AS HomeURL
+,	N2E(db.coverage)				AS Coverage
+,	N2E(db.completeness)			AS Completeness
+,	N2E(db.confidence)				AS Confidence
+,	''								AS LogoFileName
+,	N2E(db.contact_person)			AS ContactPerson
 INTO OUTFILE '__OUTPUT_DIR__/SourceDatabase.txt'
 CHARACTER SET UTF8
 FIELDS ENCLOSED BY '"' 
