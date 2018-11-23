e65ddedee4a4e0a86fe5603343e9efd3a832767b
mdoering <mdoering@gbif.org>
2018-11-24 00:19:08 +0100

    Add MycoBank

--- new-datasets.sql
+++ new-datasets.sql
@@ -2,6 +2,7 @@
 -- origin:  http://api.col.plus/vocab/datasetorigin
 --          0=EXTERNAL, 1=UPLOADED, 2=MANAGED
 -- type:  http://api.col.plus/vocab/datasettype
+--  		0=nomenclatural, 1=global, 2=regional, 3=personal, 4=other
 -- contributes_to:  http://api.col.plus/vocab/catalogue
 --          0=COL, 1=PCAT
 -- code:  http://api.col.plus/vocab/nomCode
@@ -14,4 +15,5 @@ INSERT INTO dataset (key, origin, type, contributes_to, code, title, import_freq
 ('1599', 0, 1, null, 1, 'ColDP Example', 7, 3, 'https://github.com/Sp2000/coldp/archive/master.zip'),
 ('1600', 0, 1, 0, 4, 'Neuropterida', 1, 1, 'https://github.com/Sp2000/colplus-repo/raw/master/various/Neuropterida_ACEF_CoLPlus.zip'),
 ('1601', 0, 1, 0, 4, 'ThripsWiki', 1, 1, 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/203.tar.gz'),
-('1602', 0, 1, 0, 4, 'WoRMS Amphipoda', 1, 1, 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/202.tar.gz');
+('1602', 0, 1, 0, 4, 'WoRMS Amphipoda', 1, 1, 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/202.tar.gz'),
+('1603', 0, 0, 1, 1, 'MycoBank', 7, 0, 'https://github.com/mdoering/mycobank/archive/master.zip');
