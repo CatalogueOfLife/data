6684476f6c1c78568a4b73c25cc68a90870b478f
Markus Döring <m.doering@mac.com>
2019-02-13 12:00:28 +0100

    echo progress

--- ac-export.sh
+++ ac-export.sh
@@ -23,13 +23,17 @@ while getopts ":d:h:k:o:u:" opt; do
   esac
 done
 
-echo -e "Export CoL $dataset_key from $host/$db to $export_dir\n\n"
+echo -e "Create & wipe export director $export_dir\n"
 rm -rf $export_dir
 mkdir $export_dir
 chmod 766 $export_dir
 
 # export csv files
+echo -e "Export CoL $dataset_key from $host/$db to CSV files in $export_dir\n"
 cat ac-export.sql | sed "s/{{datasetKey}}/${dataset_key}/g; s:{{dir}}:${export_dir}:g" | psql -h $host -U $user $db
 
+
 # compress
-tar czf "${export_dir}/ac-export.zip" $export_dir/*.csv
+zipfile="${export_dir}/ac-export.zip"
+echo -e "Compress CSV files into $zipfile\n"
+tar czf $zipfile $export_dir/*.csv
